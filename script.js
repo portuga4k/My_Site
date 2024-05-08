1ec855c0b33e1d94a3103a6c8c766633d7f23bcd
portuga4k <francisco.abreu195@gmail.com>
2024-05-08 19:21:03 +0100

    menu inicial estilizado
    menu inicial com algum estilo, inicio de programação com javascript e adição de uma lista de to do

---
+++ script.js
@@ -0,0 +1,11 @@
+function Play(){
+   //mudar do modo de menu para modo de jogo
+   //fazer com que apareça a Samara e os menus do jogo
+   if(getElementsByClassName("title")){
+    delete this;
+   }
+
+
+
+
+}
