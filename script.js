b3155d7473f2e17dd3c9771b036c4f9415ff4669
portuga4k <francisco.abreu195@gmail.com>
2024-05-17 11:53:02 +0100

    restruturação do código
    adição de divisões e restruturação do código de maneira a que seja mais versátil com .js e .html

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function Play(){
    //mudar do modo de menu para modo de jogo
    //fazer com que apareça a Samara e os menus do jogo
-   if(getElementsByClassName("title")){
-    delete this;
-   }
+   
+}
+
+function nextClothes(){
 
 
+}
 
+function nextAccesories(){
 
+   
 }
