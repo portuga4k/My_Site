c1fb20980e44bd446fed3236da8a92147414f3c7
portuga4k <francisco.abreu195@gmail.com>
2024-05-19 15:18:34 +0100

    estilo do menu das roupas feito
    estilo visual dos botões e do armário feito

--- script.js
+++ script.js
@@ -2,36 +2,27 @@ function Play(){
    //mudar do modo de menu para modo de jogo
    //fazer com que apareça a Samara e os menus do jogo
    var inicio = document.getElementsByClassName("title");
-   delete (inicio);
+   
 }
+
 
 function nextTops(){
-
+   
+   
 
 }
 
-
- 
 
 let topsArray = [
 
-   {      
-      "name":"pinkDress",
-      "imgSource": "/images/pink_dress.png",
-   },
+   {"name":"pinkDress", "imgSource": "/images/pink_dress.png"},
 
-   { 
-      "name":"topVerde",
-      "imgSource": "/images/Top_verde.png",
-   }
-   
+   {"name":"topVerde", "imgSource": "/images/Top_verde.png"}
+         
 ]
 
 let accesoriesArray = [
 
-   { 
-     "name": "laco",
-     "imgSource": "/images/laço.png",
-
-   }
+   {"name": "laco", "imgSource": "/images/laço.png"}
+   
 ] 
